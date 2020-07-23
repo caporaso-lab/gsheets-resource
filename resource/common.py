c15dd85e6505ee9b3ed8f96b5982301de216e2c9
Anthony Simard <als872@nau.edu>
2020-07-23 10:30:43 -0700

    Unpack and repack google auth ty google

--- resource/common.py
+++ resource/common.py
@@ -26,3 +26,11 @@ def get_file(credentials, tsv_download):
     response = session.get(tsv_download)
 
     return response
+
+def get_opts(source):
+    cred = {'type', 'project_id', 'private_key_id', 'private_key',
+            'client_email', 'client_id', 'auth_uri', 'token_uri',
+            'auth_provider_x509_cert_url', 'client_x509_cert_url'}
+    credentials = {k: v for k, v in source.items() if k in cred}
+    file_id, rev_key = source['file_id'], source['rev_key']
+    return dict(file_id=file_id, rev_key=rev_key, credentials=credentials)
